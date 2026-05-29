74c2d1dd975372b8e25e1625e98ebaa925444ab3
yasithppasindu <yasithpasindu7@gmail.com>
2026-05-29 07:01:12 +0530

    update website settings

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from "@vercel/analytics/next"
 import './index.css'
 import App from './App.jsx'
 
